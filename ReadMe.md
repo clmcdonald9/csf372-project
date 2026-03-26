21c6d14911fc9f1b347a020f79d6d185ce0d5ef8
clmcdonald9 <clmcdonald9@alaska.edu>
2026-03-25 16:52:31 -0800

    created script to seed a test database, added instructions in the ReadMe

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,7 @@
-~ The project repo ~
+# ~ The project repo ~
+
+## How to use seedTestDB.js
+
+1. run npm install to get the node modules
+2. make sure mongosh is successfully running on the default port: 27017
+3. run ```node seedTestDB.js```
---
+++ seedTestDB.js
@@ -0,0 +1,63 @@
+const mongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localHost:27017"
+const DBName = "SCProject"
+
+const users = [
+  {
+    username: "admin1",
+    password: "password123",
+    fullName: "Admin User",
+    role: "admin"
+  },
+  {
+    username: "viewer1",
+    password: "password123",
+    fullName: "Viewer One",
+    role: "viewer"
+  }
+];
+
+const movies = [
+  {
+    title: "movie1",
+    genre: "Adventure",
+    releaseDate: 2024,
+    views: 0,
+    likes: 0
+  },
+  {
+    title: "movie2",
+    genre: "Sci-Fi",
+    releaseDate: 2023,
+    views: 0,
+    likes: 0
+  }
+];
+
+async function seedTestDB() {
+  const client = new mongoClient(url);
+  try {
+    await client.connect(url);
+    console.log("connected to MongoDB")
+
+    const db = client.db(DBName);
+
+    await db.collection("users").deleteMany({});
+    await db.collection("movies").deleteMany({});
+
+    await db.collection("users").insertMany(users);
+    await db.collection("movies").insertMany(movies);
+
+    console.log("seed successful");
+
+  }
+  catch (error) {
+    console.error("seed failed: ", error);
+  }
+  finally {
+    await client.close();
+    console.log("connection closed")
+  }
+}
+
+seedTestDB();
