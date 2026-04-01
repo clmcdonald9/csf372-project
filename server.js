521a73b11d291be9fb8a9f850eb93c0d07c84cc7
clmcdonald9 <clmcdonald9@alaska.edu>
2026-04-01 13:02:25 -0800

    implemented local host server

---
+++ server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const publicDirPath = path.join(__dirname, 'public');
+
+app.use(express.static(publicDirPath));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(publicDirPath, "Home.html"));
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
