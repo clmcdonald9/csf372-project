9ec2f866a31c747e3b46b7b01062f273e8ec4647
clmcdonald9 <clmcdonald9@alaska.edu>
2026-04-02 13:32:21 -0800

    checkpoint: began connecting mongodb

--- public/Login.js
+++ public/Login.js
@@ -3,22 +3,8 @@ const usernameField = document.getElementById('text_username');
 const passwordField = document.getElementById('password_user_password');
 const errorMessage = document.getElementById('p_login_error_message');
 
-// Regular expression to validate username and password: 
-// 8-16 characters, at least one lowercase letter, one uppercase letter,
-//  one digit, and one special character (!@#$%^&*?_.).
-const loginRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_.])[a-zA-Z0-9!@#$%^&*?_.]{8,16}$/;
-
-
-// Not sure this will be necessary for ordinary users,other than when resetting password. 
-// but eventually maybe this can be used when an admin creates a new user account?
-form.addEventListener('submit', function(event) { 
-    errorMessage.style.display = 'none';
-
-    const username = usernameField.value;
-    const password = passwordField.value;
-
-    if (!loginRegex.test(username) || !loginRegex.test(password)) { 
-        event.preventDefault();
-        errorMessage.style.display = 'block';
-    }
-});
+if (form && usernameField && passwordField && errorMessage) {
+    form.addEventListener('submit', function(event) { 
+        
+    });
+}
--- server.js
+++ server.js
@@ -1,8 +1,26 @@
+const { MongoClient } = require('mongodb');
 const path = require('path');
 const express = require('express');
 const app = express();
 
+const port = 3000;
 const publicDirPath = path.join(__dirname, 'public');
+
+const url = "mongodb://localHost:27017";
+const DBName = "SCProject";
+
+const client = new MongoClient(url);
+
+async function connectToDB() {
+    try {
+        await client.connect(url);
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
+
+connectToDB();
 
 app.use(express.static(publicDirPath));
 
@@ -10,6 +28,6 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(publicDirPath, "Home.html"));
 });
 
-app.listen(3000, "0.0.0.0", () => {
+app.listen(port, "0.0.0.0", () => {
     console.log('Server is up on port 3000');
 });
