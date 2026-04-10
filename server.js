95d31e392b0bfc239f8923a15cf0de3b419031c5
clmcdonald9 <clmcdonald9@alaska.edu>
2026-04-10 13:30:17 -0800

    imported express-session and added it to the server.js

--- server.js
+++ server.js
@@ -2,10 +2,22 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const session = require('express-session');
 const { connectToDB } = require('./db')
+
 
 const PORT = 3000;
 const PUBLIC_DIRECTORY_PATH = path.join(__dirname, 'public');
+
+app.use(session({
+    secret: `secret-key-that-normally-should-be-in-env-but-i-think-its-
+            fine-for-the-class-project-probably-and-also-i-think-he-said-
+            we-didnt-even-need-to-use-sessions-but-i-think-it-will-make-some-
+            things-easier`,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }
+}));
 
 app.use(express.static(PUBLIC_DIRECTORY_PATH));
 app.use(express.json());
